b0ffd604c6d722b04a04dc42206cc8979b21be0a
StoneHao <1483027297@qq.com>
2019-01-26 00:03:03 +0800

    03-设置未登录拦截/登录校验/登陆错误信息提示

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,21 +2,24 @@
 <template>
   <div class="login">
     <el-form label-position="top" label-width="80px" :model="login" :rules="rules" ref="loginForm">
-      <el-form-item label="名称" prop="username">
+      <el-form-item label="用户名" prop="username">
         <el-input v-model="login.username"></el-input>
       </el-form-item>
-      <el-form-item label="活动区域" prop="password">
-        <el-input v-model="login.password"></el-input>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="login.password" type="password"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
-        <el-button @click="resetForm('loginForm')">重置</el-button>
+        <el-button type="primary" @click="submitForm">登录</el-button>
+        <el-button @click="resetForm">重置</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
+  // 导入 axios
+  import axios from 'axios';
+
   /**
    * el-form
    * label-position="top" 设置label的位置
@@ -36,7 +39,7 @@
         },
         rules: {
           username: [
-            {required: true, message: '请输入活动名称', trigger: 'blur'},
+            {required: true, message: '请输入用户名', trigger: 'blur'},
             {min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur'},
           ],
           password: [
@@ -47,11 +50,43 @@
       };
     },
     methods: {
-      submitForm (ruleForm) {
-        console.log(this.login.username);
+      /* 1 校验用户名和密码
+       * 2 校验成功 => 向后台发送请求
+       * 3 校验失败 => 提示用户
+       * 4 后台返回数据 判断是否成功
+       * 5 成功=> 跳转首页  失败=> 提示信息 tip 使用element中错误提示组件
+       * 6 跳转主页后 修改主页的样式
+       * 7 在登陆请求返回的数据中有一个token 这是判断是否登陆成功的重要标志
+       *    如果有表示登陆成功 在获取返回数据的同时 将token存入到localStorage中
+       *    方便路由中的全局守卫可以获取到
+       * */
+      submitForm () {
+        // 对整个表单进行校验的方法，参数为一个回调函数。
+        // 参数 valid  是否校验成功
+        this.$refs.loginForm.validate(valid => {
+          if (valid) {
+            axios.post('http://localhost:8888/api/private/v1/login', this.login).then(result => {
+              const {data, meta} = result.data;
+              if (meta.msg === '登录成功') {
+                // 登陆成功 存储token 跳转首页
+                localStorage.setItem('token', data.token);
+                this.$router.push('/home');
+              }
+              else {
+                // 登陆失败 给出提示
+                this.$message({
+                  message: '用户名或者密码错误',
+                  type: 'error',
+                  duration: 2000,
+                });
+              }
+            });
+          }
+
+        });
       },
       resetForm () {
-        console.log(this.login.password);
+        this.$refs.loginForm.resetFields();
       },
     },
   };
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,46 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '../components/Login.vue';
+import Home from '../components/Home.vue';
 //  安装路由插件 在webpack这样的模块化系统中，必须执行这一步 如果通过script引入则不需要
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
+    {
+      path: '/',
+      name: 'index',
+      redirect: '/login',
+    },
     {
       path: '/login',
       name: 'Login',
       component: Login,
     },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+    },
   ],
 });
+/* 在路由文件中配置全局守卫
+ *  0 访问是登陆页面直接next 不是的话就需要判断是否登陆成功
+ *  1 当用户登陆成功，继续调用next()方法 不在拦截
+ *  2 当用户登陆不成功，默认跳转next('/login')页面 进行拦截
+ *     获取存储在localStorage中的token 以此来判断是否登陆成功
+ * */
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') {
+    next();
+  } else {
+    let token = localStorage.getItem('token');
+    if (token) {
+      next();
+    } else {
+      next('/login');
+    }
+  }
+});
+
+export default router;
